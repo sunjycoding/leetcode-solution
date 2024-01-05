f7c249017c0ed01c270cd76d8a6f5b45cf79b34a
sunjy <sunjyleisure@gmail.com>
2024-01-04 22:14:11 -0800

    feat: day004

---
+++ src/day004/TwoSumTwo.java
@@ -0,0 +1,32 @@
+package day004;
+
+import java.util.Arrays;
+
+/**
+ * 167. Two Sum II - Input Array Is Sorted
+ *
+ * @author created by sunjy on 1/4/24
+ */
+public class TwoSumTwo {
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 3, 4}, 6)));
+    }
+
+}
