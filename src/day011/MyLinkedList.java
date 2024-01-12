4939654c7966cacf9d74d16ee70735e2d7eccc66
sunjy <sunjyleisure@gmail.com>
2024-01-11 22:02:05 -0800

    feat: day011

---
+++ src/day011/MyLinkedList.java
@@ -0,0 +1,94 @@
+package day011;
+
+/**
+ * 707. Design Linked List
+ *
+ * @author created by sunjy on 1/11/24
+ */
+public class MyLinkedList {
+
+    private ListNode head;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        ListNode current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.val;
+    }
+
+    public void addAtHead(int val) {
+        ListNode listNode = new ListNode(val);
+        listNode.next = head;
+        head = listNode;
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        ListNode listNode = new ListNode(val);
+        if (head == null) {
+            head = listNode;
+        } else {
+            ListNode current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = listNode;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) {
+            return;
+        }
+        if (index == 0) {
+            addAtHead(val);
+        } else {
+            ListNode newNode = new ListNode(val);
+            ListNode current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+            size++;
+        }
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        if (index == 0) {
+            head = head.next;
+        } else {
+            ListNode current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+        size--;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+}
