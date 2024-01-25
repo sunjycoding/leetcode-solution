8f660a3592e3f3982e53d4666a11c85b7f7fa8c7
sunjy <sunjyleisure@gmail.com>
2024-01-24 20:16:16 -0800

    feat: day024

---
+++ src/day024/RandomizedSet.java
@@ -0,0 +1,50 @@
+package day024;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ * 380. Insert Delete GetRandom O(1)
+ *
+ * @author created by sunjy on 1/24/24
+ */
+public class RandomizedSet {
+
+    private ArrayList<Integer> list;
+    private HashMap<Integer, Integer> map;
+    private Random random;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+        int index = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(index, lastElement);
+        map.put(lastElement, index);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
+    }
+
+}
