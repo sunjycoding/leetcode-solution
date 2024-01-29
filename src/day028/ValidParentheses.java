86bc681aec95274904220c0bf04219fc8d6a26d6
sunjy <sunjyleisure@gmail.com>
2024-01-28 18:14:06 -0800

    feat: day028

---
+++ src/day028/ValidParentheses.java
@@ -0,0 +1,37 @@
+package day028;
+
+import java.util.Stack;
+
+/**
+ * 20. Valid Parentheses
+ *
+ * @author created by sunjy on 1/28/24
+ */
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+    }
+
+}
