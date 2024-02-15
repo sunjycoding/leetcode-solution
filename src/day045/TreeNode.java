c6d225df513c8893e9a9caa399b2211b0b8dd279
sunjy <sunjyleisure@gmail.com>
2024-02-14 23:18:27 -0800

    feat: day045

---
+++ src/day045/TreeNode.java
@@ -0,0 +1,25 @@
+package day045;
+
+/**
+ * @author created by sunjy on 2/14/24
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
