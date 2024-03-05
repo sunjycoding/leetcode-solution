1e65d7245a445abb7dd055d7a144e6bdfa8db2d2
sunjy <sunjyleisure@gmail.com>
2024-03-04 22:12:05 -0800

    feat: day064

---
+++ src/day064/MinimumLengthOfStringAfterDeletingSimilarEnds.java
@@ -0,0 +1,29 @@
+package day064;
+
+/**
+ * 1750. Minimum Length of String After Deleting Similar Ends
+ *
+ * @author created by sunjy on 3/4/24
+ */
+public class MinimumLengthOfStringAfterDeletingSimilarEnds {
+
+    public int minimumLength(String s) {
+        int left = 0, right = s.length() - 1;
+
+        // Loop until the two pointers meet
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            char currentChar = s.charAt(left);
+            // Move the left pointer to the right, skipping over all the same characters
+            while (left <= right && s.charAt(left) == currentChar) {
+                left++;
+            }
+            // Move the right pointer to the left, skipping over all the same characters
+            while (left <= right && s.charAt(right) == currentChar) {
+                right--;
+            }
+        }
+        // Calculate the remaining length of the string
+        return right - left + 1;
+    }
+
+}
