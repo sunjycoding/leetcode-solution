cf2a98578f0064fddb2e1b92937aa76636474d07
sunjy <sunjyleisure@gmail.com>
2024-03-06 22:09:05 -0800

    feat: day066

---
+++ src/day066/MiddleOfTheLinkedList.java
@@ -0,0 +1,28 @@
+package day066;
+
+/**
+ * 876. Middle of the Linked List
+ *
+ * @author created by sunjy on 3/6/24
+ */
+public class MiddleOfTheLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Move fast pointer two steps and slow pointer one step at a time
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // When fast pointer reaches the end, slow pointer will be at the middle
+        return slow;
+    }
+
+}
