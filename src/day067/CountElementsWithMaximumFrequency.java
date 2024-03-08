7ba15d04d422272402894701eb126bac564d6f12
sunjy <sunjyleisure@gmail.com>
2024-03-07 22:14:30 -0800

    feat: day067

---
+++ src/day067/CountElementsWithMaximumFrequency.java
@@ -0,0 +1,36 @@
+package day067;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 3005. Count Elements With Maximum Frequency
+ *
+ * @author created by sunjy on 3/7/24
+ */
+public class CountElementsWithMaximumFrequency {
+
+    public int maxFrequencyElements(int[] nums) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        int maxFrequency = 0;
+
+        // Populate the frequency map
+        for (int num : nums) {
+            int currentFrequency = frequencyMap.getOrDefault(num, 0) + 1;
+            frequencyMap.put(num, currentFrequency);
+            // Update maxFrequency if the current number's frequency is higher
+            maxFrequency = Math.max(maxFrequency, currentFrequency);
+        }
+
+        int totalMaxFreq = 0;
+        for (int freq : frequencyMap.values()) {
+            // Sum frequencies of elements that have the maximum frequency
+            if (freq == maxFrequency) {
+                totalMaxFreq += freq;
+            }
+        }
+
+        return totalMaxFreq;
+    }
+
+}
---
+++ src/day067/LongestPalindromicSubstring.java
@@ -0,0 +1,41 @@
+package day067;
+
+/**
+ * 5. Longest Palindromic Substring
+ *
+ * @author created by sunjy on 3/7/24
+ */
+public class LongestPalindromicSubstring {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        String longestPalindrome = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String substr = s.substring(i, j + 1);
+                if (isPalindrome(substr) && substr.length() > longestPalindrome.length()) {
+                    longestPalindrome = substr;
+                }
+            }
+        }
+        return longestPalindrome;
+    }
+
+    // Helper method to check if a string is a palindrome
+    private boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
