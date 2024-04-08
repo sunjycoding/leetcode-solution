65f102709d34abd6414b2c7438899b46eb782874
sunjy <sunjyleisure@gmail.com>
2024-04-07 22:43:35 -0700

    feat: day098

---
+++ src/day098/NumberOfStudentsUnableToEatLunch.java
@@ -0,0 +1,33 @@
+package day098;
+
+/**
+ * 1700. Number of Students Unable to Eat Lunch
+ *
+ * @author created by sunjy on 4/7/24
+ */
+public class NumberOfStudentsUnableToEatLunch {
+
+    public int countStudents(int[] students, int[] sandwiches) {
+        // Count[0] for circular (0), count[1] for square (1)
+        int[] count = new int[2];
+
+        // Count the preferences of all students
+        for (int student : students) {
+            count[student]++;
+        }
+
+        // Iterate through the sandwiches from the top of the stack
+        for (int sandwich : sandwiches) {
+            // If no student wants the current type of sandwich, break
+            if (count[sandwich] == 0) {
+                break;
+            }
+            // Otherwise, one student takes a sandwich and leaves
+            count[sandwich]--;
+        }
+
+        // The students unable to eat are those remaining
+        return count[0] + count[1];
+    }
+
+}
