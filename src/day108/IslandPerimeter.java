1d7eb6bdb8dfac75c95549550d6cee24efc9d12c
sunjy <sunjyleisure@gmail.com>
2024-04-17 21:14:08 -0700

    feat: day108

---
+++ src/day108/IslandPerimeter.java
@@ -0,0 +1,45 @@
+package day108;
+
+/**
+ * 463. Island Perimeter
+ *
+ * @author created by sunjy on 4/17/24
+ */
+public class IslandPerimeter {
+
+    public static int islandPerimeter(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int perimeter = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    // Check top
+                    if (i == 0 || grid[i - 1][j] == 0) {
+                        perimeter++;
+                    }
+                    // Check bottom
+                    if (i == rows - 1 || grid[i + 1][j] == 0) {
+                        perimeter++;
+                    }
+                    // Check left
+                    if (j == 0 || grid[i][j - 1] == 0) {
+                        perimeter++;
+                    }
+                    // Check right
+                    if (j == cols - 1 || grid[i][j + 1] == 0) {
+                        perimeter++;
+                    }
+                }
+            }
+        }
+
+        return perimeter;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{};
+        System.out.println(islandPerimeter(grid));
+    }
+}
