dfcd96077109ffa7cd1214871f74cc588bc8838b
sunjy <sunjyleisure@gmail.com>
2024-05-17 23:15:33 -0700

    feat: day137

---
+++ src/day137/DistributeCoinsInBinaryTree.java
@@ -0,0 +1,28 @@
+package day137;
+
+/**
+ * 979. Distribute Coins in Binary Tree
+ * @author created by sunjy on 5/17/24
+ */
+public class DistributeCoinsInBinaryTree {
+
+    private int moves = 0;
+
+    public int distributeCoins(TreeNode root) {
+        distributeCoinsHelper(root);
+        return moves;
+    }
+
+    private int distributeCoinsHelper(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftCoins = distributeCoinsHelper(node.left);
+        int rightCoins = distributeCoinsHelper(node.right);
+        moves += Math.abs(leftCoins) + Math.abs(rightCoins);
+
+        return node.val + leftCoins + rightCoins - 1;
+    }
+
+}
