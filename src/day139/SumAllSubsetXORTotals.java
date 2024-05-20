1bfe4824a99272ef4933ee444df3bf91fbd530e1
sunjy <sunjyleisure@gmail.com>
2024-05-19 21:25:55 -0700

    feat: day139

---
+++ src/day139/SumAllSubsetXORTotals.java
@@ -0,0 +1,28 @@
+package day139;
+
+/**
+ * 1863. Sum of All Subset XOR Totals
+ *
+ * @author created by sunjy on 5/19/24
+ */
+public class SumAllSubsetXORTotals {
+
+    public int subsetXORSum(int[] nums) {
+        return subsetXOR(nums, 0, 0);
+    }
+
+    private int subsetXOR(int[] nums, int index, int currentXOR) {
+        if (index == nums.length) {
+            // We've considered all elements, return the current XOR total
+            return currentXOR;
+        } else {
+            // Recurse by including the current element in the XOR
+            int include = subsetXOR(nums, index + 1, currentXOR ^ nums[index]);
+            // Recurse by excluding the current element from the XOR
+            int exclude = subsetXOR(nums, index + 1, currentXOR);
+            // Return the sum of XORs from including and excluding the current element
+            return include + exclude;
+        }
+    }
+
+}
---
+++ src/day139/TreeNode.java
@@ -0,0 +1,25 @@
+package day139;
+
+/**
+ * @author created by sunjy on 5/19/24
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
