6ec1bd7110c6a06ffadc39e372987bdb03f17544
sunjy <sunjyleisure@gmail.com>
2024-06-12 00:39:52 -0700

    feat: day162

---
+++ src/day162/SortColors.java
@@ -0,0 +1,33 @@
+package day162;
+
+/**
+ * 75. Sort Colors
+ *
+ * @author created by sunjy on 6/12/24
+ */
+public class SortColors {
+
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // Swap nums[low] and nums[mid]
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 2) {
+                // Swap nums[mid] and nums[high]
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+
+}
